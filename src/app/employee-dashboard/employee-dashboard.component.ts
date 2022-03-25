e9eecb8852f79b25e00a49e69c08495920d231bf
Mohit Shukla <shuklamohit1987@gmail.com>
2022-03-25 12:20:09 +0530

    put and delete

--- src/app/employee-dashboard/employee-dashboard.component.ts
+++ src/app/employee-dashboard/employee-dashboard.component.ts
@@ -13,6 +13,8 @@ export class EmployeeDashboardComponent implements OnInit {
   formvalue!:FormGroup;
   employeeModelObj:EmployeeModel=new EmployeeModel();
   employeeData!:any;
+  showAdd!:boolean;
+  showUpdate!:boolean;
   constructor(private formbuilder:FormBuilder, private api:ApiService) 
   {
     
@@ -27,6 +29,12 @@ export class EmployeeDashboardComponent implements OnInit {
       salary:['']
     })
     this.getEmployeeDetails();
+  }
+  clickAddEmployee()
+  {
+    this.formvalue.reset();
+    this.showAdd=true;
+    this.showUpdate=false;
   }
 postEmployeeDetails()
 {
@@ -57,5 +65,42 @@ this.api.getEmployee()
 .subscribe(res=>{
   this.employeeData=res;
 })
+}
+
+deleteEmployee(emp:any)
+{
+  this.api.deleteEmployee(emp.id)
+  .subscribe(res=>{
+    alert("Record Deleted Successfully");
+    this.getEmployeeDetails();
+  })
+}
+onEdit(emp:any)
+{
+  this.showAdd=false;
+  this.showUpdate=true;
+  this.employeeModelObj.id=emp.id;
+  this.formvalue.controls['firstname'].setValue(emp.firstname);
+  this.formvalue.controls['lastname'].setValue(emp.lastname);
+  this.formvalue.controls['email'].setValue(emp.email);
+  this.formvalue.controls['mobile'].setValue(emp.mobile);
+  this.formvalue.controls['salary'].setValue(emp.salary);
+}
+updateEmployeeDetails()
+{
+  this.employeeModelObj.firstname=this.formvalue.value.firstname;
+  this.employeeModelObj.lastname=this.formvalue.value.lastname;
+  this.employeeModelObj.email=this.formvalue.value.email;
+  this.employeeModelObj.mobile=this.formvalue.value.mobile;
+  this.employeeModelObj.salary=this.formvalue.value.salary;
+
+  this.api.updateEmployee(this.employeeModelObj,this.employeeModelObj.id)
+  .subscribe(res=>{
+    alert("Record Updated Successfully");
+    let ref=document.getElementById('cancel')
+    ref?.click();
+    this.formvalue.reset();
+    this.getEmployeeDetails();
+  })
 }
 }
--- src/app/shared/api.service.ts
+++ src/app/shared/api.service.ts
@@ -25,4 +25,18 @@ export class ApiService {
       return res;
     }))
   }
+  updateEmployee(data:any,id:number)
+  {
+    return this.http.put<any>("http://localhost:3000/posts/"+id,data)
+    .pipe(map((res:any)=>{
+      return res;
+    }))
+  }
+  deleteEmployee(id:number)
+  {
+    return this.http.delete<any>("http://localhost:3000/posts/"+id)
+    .pipe(map((res:any)=>{
+      return res;
+    }))
+  }
 }
